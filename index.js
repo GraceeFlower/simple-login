a808b1fe19a95f80456bf5795e91e124ece17079
GraceeFlower <1491644103@qq.com>
2019-12-15 23:12:19 +0800

    [WangMin] feat: create a simple login page with the name&password checking

---
+++ index.js
@@ -0,0 +1,17 @@
+var objectSubmit = document.getElementsByTagName("button")[0];
+var objectUser = document.getElementById("username");
+var objectPassword = document.getElementById("password");
+
+objectSubmit.addEventListener("click", checkAccount, false);
+function checkAccount() {
+  if (objectUser.value === "123" && objectPassword.value === "123") {
+    alert("You are the apple of my eyes.");
+  } else if (objectPassword.value !== "123" && objectUser.value === "123") {
+    alert("Please enter the right password!");
+    objectPassword.value = null;
+  } else {
+    alert("Please enter the right name!");
+    objectPassword.value = null;
+    objectUser.value = null;
+  }
+}

e6414d2faf7a57780f9e849f8b5809e90d027b68
GraceeFlower <1491644103@qq.com>
2019-12-20 00:48:54 +0800

    [WangMin] refactor: save the right data about the page after submit

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ function checkAccount() {
     alert("Welcome! You are the apple of my eyes.");
   } else if (objectPassword.value !== "123" && objectUser.value === "123") {
     alert("Please enter the right password!");
+    objectPassword.value = null;
   } else {
     alert("Please enter the right name!");
+    objectPassword.value = null;
+    objectUser.value = null;
   }
 }

5d84e0f19d15b5fcbdda8e378b7b1445acd10cfb
GraceeFlower <1491644103@qq.com>
2019-12-20 00:16:27 +0800

    [WangMin] refactor: change the div into a form to submit the data

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-var objectSubmit = document.getElementsByTagName("button")[0];
 var objectUser = document.getElementById("username");
 var objectPassword = document.getElementById("password");
+var loginForm = document.getElementById("login")
 
-objectSubmit.addEventListener("click", checkAccount, false);
+loginForm.addEventListener("submit", checkAccount, false);
 function checkAccount() {
   if (objectUser.value === "123" && objectPassword.value === "123") {
     alert("Welcome! You are the apple of my eyes.");
-    objectSubmit.style.backgroundColor = "#ddd";
-    objectSubmit.setAttribute("disabled", "disabled");
-    objectSubmit.style.cursor = "not-allowed";
   } else if (objectPassword.value !== "123" && objectUser.value === "123") {
     alert("Please enter the right password!");
-    objectPassword.value = null;
   } else {
     alert("Please enter the right name!");
-    objectPassword.value = null;
-    objectUser.value = null;
   }
 }
